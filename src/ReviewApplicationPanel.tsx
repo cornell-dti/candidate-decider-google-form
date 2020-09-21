b949e64e30015a2a2443eed040b49683ed47d91b
Sam <sam@developersam.com>
2020-09-21 16:48:06 -0400

    Support attaching comments to candidates

--- src/ReviewApplicationPanel.tsx
+++ src/ReviewApplicationPanel.tsx
@@ -5,7 +5,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Switch from '@material-ui/core/Switch';
-import { SheetData, Rating, Ratings } from './types';
+import { SheetData, Rating, Ratings, Comments } from './types';
 import SingleCandidateViewer from './SingleCandidateViewer';
 import SingleCandidateReviewer from './SingleCandidateReviewer';
 import styles from './Reviewer.module.css';
@@ -13,22 +13,26 @@ import styles from './Reviewer.module.css';
 type Props = {
   readonly sheetData: SheetData;
   readonly ratings: Ratings;
+  readonly comments: Comments;
   readonly candidateId: number;
   readonly updateCandidateId: (updater: number | ((previous: number) => number)) => void;
   readonly showOthers: boolean;
   readonly onToggleShowOthers: () => void;
   readonly onRatingChange: (updatedRating: Rating | null) => void;
+  readonly onCommentChange: (comment: string) => void;
   readonly className: string;
 };
 
 const ReviewApplicationPanel = ({
   sheetData: { header, content },
   ratings,
+  comments,
   candidateId,
   updateCandidateId,
   showOthers,
   onToggleShowOthers,
   onRatingChange,
+  onCommentChange,
   className
 }: Props): ReactElement => {
   const previous = () => updateCandidateId(id => id - 1);
@@ -65,8 +69,11 @@ const ReviewApplicationPanel = ({
           <span>Show other people&apos;s votes</span>
         </div>
         <SingleCandidateReviewer
+          candidateId={candidateId}
           rating={ratings[candidateId] ?? null}
+          comment={comments[candidateId] ?? ''}
           onRatingChange={updatedRating => onRatingChange(updatedRating)}
+          onCommentChange={updatedComment => onCommentChange(updatedComment)}
         />
       </div>
       <div className={styles.Section}>
--- src/SingleCandidateReviewer.tsx
+++ src/SingleCandidateReviewer.tsx
@@ -1,18 +1,53 @@
-import React, { ReactElement, ChangeEvent } from 'react';
+import React, { ReactElement, ChangeEvent, useState } from 'react';
+import Button from '@material-ui/core/Button';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
 import { Rating } from './types';
 import { ratingsText, ratingsColors } from './ratings-util';
+import styles from './SingleCandidateReviewer.module.css';
 
-type Props = {
-  readonly rating: Rating | null;
-  readonly onRatingChange: (rating: Rating | null) => void;
+type CommentEditorProps = {
+  readonly comment: string;
+  readonly onCommentChange: (comment: string) => void;
 };
 
-const SingleCandidateReviewer = ({ rating, onRatingChange }: Props): ReactElement => {
-  const onChange = (event: ChangeEvent<HTMLInputElement>): void => {
+const CommentEditor = ({ comment, onCommentChange }: CommentEditorProps): ReactElement => {
+  const [currentComment, setCurrentComment] = useState(comment);
+
+  return (
+    <div className={styles.CommentEditor}>
+      <TextField
+        className={styles.CommentEditorTextInput}
+        label="My comment"
+        value={currentComment}
+        onChange={event => setCurrentComment(event.currentTarget.value)}
+      />
+      <Button variant="contained" color="primary" onClick={() => onCommentChange(currentComment)}>
+        Save Comment
+      </Button>
+    </div>
+  );
+};
+
+type Props = {
+  readonly candidateId: number;
+  readonly rating: Rating | null;
+  readonly comment: string;
+  readonly onRatingChange: (rating: Rating | null) => void;
+  readonly onCommentChange: (comment: string) => void;
+};
+
+const SingleCandidateReviewer = ({
+  candidateId,
+  rating,
+  comment,
+  onRatingChange,
+  onCommentChange
+}: Props): ReactElement => {
+  const onRatingRadioChange = (event: ChangeEvent<HTMLInputElement>): void => {
     // eslint-disable-next-line prefer-destructuring
     const value = event.currentTarget.value;
     if (value === '') {
@@ -24,7 +59,7 @@ const SingleCandidateReviewer = ({ rating, onRatingChange }: Props): ReactElemen
   return (
     <div>
       <FormControl component="fieldset">
-        <RadioGroup row value={rating ?? ''} onChange={onChange}>
+        <RadioGroup row value={rating ?? ''} onChange={onRatingRadioChange}>
           {ratingsText.map((text, id) => (
             <FormControlLabel
               key={text}
@@ -41,6 +76,7 @@ const SingleCandidateReviewer = ({ rating, onRatingChange }: Props): ReactElemen
             labelPlacement="top"
           />
         </RadioGroup>
+        <CommentEditor key={candidateId} comment={comment} onCommentChange={onCommentChange} />
       </FormControl>
     </div>
   );
